758bc24d43287ee31471f93695d1897aff6aa41f
YekaterinaPavlenko <pavluka1605@gmail.com>
2020-10-04 09:14:21 +0300

    Добавляет 6,7,8,9 задания

--- task6.js
+++ task6.js
@@ -3,9 +3,7 @@ function mapArray(array) {
   const numbers = new Array(array.length);
   for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
-    const arrFast = array[i] * 10;
-    new Array();
-    console.log(arrFast);
+    numbers[i] = array[i] * 10;
   }
   return numbers;
 }
@@ -13,5 +11,5 @@ function mapArray(array) {
 console.log(mapArray([-2, 0, 2]));
 // [-20, 0, 20]
 
-//console.log(mapArray([-2.5, 0, 2.5]));
+console.log(mapArray([-2.5, 0, 2.5]));
 // [-25, 0, 25]
